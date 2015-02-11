044c6254aef060641eb7e5e0629df2a87c5b8c7a
tremogliec <tremogliec@gmail.com>
2015-02-11 08:41:07 -0500

    Derry Township Build 1.0

---
+++ index.php
@@ -0,0 +1,163 @@
+<?php include 'includes/header.php';?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <meta name="format-detection" content="telephone=no">
+
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <link href='http://fonts.googleapis.com/css?family=Alegreya:400,400italic,700,700italic,900|Lato:400,400italic,700' rel='stylesheet' type='text/css'>
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+            <![endif]-->
+
+            <!--Google Analytics-->
+
+            <!---->
+        </head>
+        <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+            <![endif]-->
+
+            <div class="navigation-container">
+                <div class="container">
+                    <nav>
+                        <a href="">Community</a>&nbsp;<div class="circle"></div>
+                        <a href="">Government</a>&nbsp;<div class="circle"></div>
+                        <a href="">Departments</a>&nbsp;<div class="circle"></div>
+                        <a href="">Resident Services</a>&nbsp;<div class="circle"></div>
+                        <a href="">Business</a>&nbsp;<div class="circle"></div>
+                        <a href="">Resources</a>
+                    </nav>
+          <!--           <div class="search">
+                        <form class="search-bar" role="search">
+                          <input type="search" placeholder="Enter Search" />
+                          <button type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </form>
+                </div> -->
+            </div>
+        </div>
+
+        <div class="header-container cf">
+            <div class="container">
+                <div class="grid-2 logo no-margin"><img src="images/Derry-Township-Logo.png" alt="Derry Township Hershey PA"></div>
+                <div class="grid-3 header no-margin"><h2 class="main-title">Hershey... Where It's Just Sweeter</h2><span>We can't think of a better place to call home</span></div>
+                <div class="grid-1 social no-margin">
+                    <a href=""><i class="facebook fa fa-facebook fa-lg"></i></a>
+                    <a href=""><i class="twitter fa fa-twitter fa-lg"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="slant"></div>
+        <div class="header-info cf">
+                <div class="container">
+                    <div class="grid-3">
+                    <h1 class="home">Welcome to Derry Township,</h1>
+                        <p>Best known for world famous HERSHEY’S Milk Chocolate, Hershey thrives today on what began as a vision of Milton S. Hershey to build a model town to meet the basic, recreational, and cultural needs of the factory workers. His values live on and are embraced by the elected officials, residents and business owners of this great community. There is a pride that comes with saying, “I live in Hershey.”</p>
+                    </div>
+                    <div class="grid-3 bottom-box">
+                        <h3>Hershey Gardens</h3>
+                        <img src="images/header-home-img.jpg">
+                    </div>
+                </div>
+        </div>
+
+        <div class="side-bar grid-2 no-margin cf">
+            <div class="container">
+                <ul>
+                    <li><a href="">Ordinances & Resolutions<span class="fa fa-angle-double-right"></span></a></li>
+                    <li><a href="">Minutes & Agendas<span class="fa fa-angle-double-right"></span></a></li>
+                    <li><a href="">Departments<span class="fa fa-angle-double-right"></span></a></li>
+                    <li><a href="">Job Opportunities<span class="fa fa-angle-double-right"></span></a></li>
+                    <li><a href="">Comprehensive Plan<span class="fa fa-angle-double-right"></span></a></li>
+                    <li><a href="">Newsletter Signup<span class="fa fa-angle-double-right"></span></a></li>
+                </ul>
+            </div>
+
+            <div class="container">
+            <div class="emergency-alert-container">
+                <h2>Emergency Alert Signup</h2>
+                <img src="images/code-red.png" alt="Code Red Keeping Citizens informed">
+                <a href="" class="sign-up">Signup for Code Red<span class="fa fa-angle-double-right"></span></a>
+            </div>
+            <div class="latest-events-container">
+            <h2>Latest Events</h2>
+            <ul>
+                <li>
+                    <span>Parks and Recreation Advisory Board</span>
+                    <p>01-14-2015 | 07:00pm</p>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <span>Parks and Recreation Advisory Board</span>
+                    <p>01-14-2015 | 07:00pm</p>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <span>Parks and Recreation Advisory Board</span>
+                    <p>01-14-2015 | 07:00pm</p>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <span>Parks and Recreation Advisory Board</span>
+                    <p>01-14-2015 | 07:00pm</p>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <span>Parks and Recreation Advisory Board</span>
+                    <p>01-14-2015 | 07:00pm</p>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#">View all events<span class="fa fa-angle-double-right"></span></a>
+                </li>
+            </ul>
+            </div>
+
+            <div class="contact-us-container">
+                <h2>Contact us</h2>
+                <span>600 Clearwater Road</span>
+                <span>Hershey, PA 17033</span>
+                <span>Phone: 717.533.2057</span>
+                <span>Fax: 717.533.7174</span>
+            </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="main-content grid-4 no-margin cf">
+            <div class="container">
+                <div class="update">
+                    <span>Service Update:</span>
+                    <p>Christmas trees will be collected beginning Monday, January 5 through January 23, 2015</p>
+                </div>
+                <h4>Current News</h4>
+            </div>
+        </div>
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+    </body>
+    </html>
+    <?php include 'includes/footer.php';?>
